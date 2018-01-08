4c72bf8b8bfe61a2b4f02b61f6e6f8223efbcf73
Julien Quartier <julien.quartier@bluewin.ch>
2018-01-08 14:10:25 +0100

    header

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
+/*
+ -----------------------------------------------------------------------------------
+ Laboratoire : Labo04
+ Fichier     : main.cpp
+ Auteur(s)   : Hugo Jeanneret, Julien Quartier
+ Date        : 08.01.2018
+ But         :
+
+ Remarque(s) :
+
+ Compilateur : MinGW-g++ 6.3.0
+ -----------------------------------------------------------------------------------
+ */
+
+
+#include <cstdlib>
 #include <iostream>
+
+using namespace std;
+
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+
+    
+    return EXIT_SUCCESS;}
