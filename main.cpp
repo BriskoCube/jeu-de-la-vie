bd643b96a3c2582fcc8d25461f24de88162327ef
Julien Quartier <julien.quartier@bluewin.ch>
2018-01-10 13:53:28 +0100

    Const

--- main.cpp
+++ main.cpp
@@ -24,6 +24,21 @@ using namespace std;
 const size_t NB_COLL = 11;
 const size_t NB_LIGNES = 10;
 
+const unsigned ITERATION = 8;
+
+const char CAR_EN_VIE = 'X';
+const char CAR_MORT = '.';
+
+//Doit être impaire
+const size_t ZONE_INFLUANCE = 3;
+
+vector<vector<int>> VALEUR_PAR_DEFAULT {
+        {4,4},
+        {4,5},
+        {4,6},
+        {5,5}
+};
+
 void initialisationGrilleJeu(vector<vector<bool>> &grilleJeu);
 void afficherGrilleJeu(const vector<vector<bool>> &grilleJeu);
 void evolution(const vector<vector<bool>> &grilleJeu, vector<vector<bool>> &anticipation);
@@ -35,34 +50,23 @@ int main() {
 
     // False : vide, True : vie
     vector<vector<bool>> grilleJeu(NB_LIGNES, vector<bool>(NB_COLL, false));
-
-
     vector<vector<bool>> anticipation(NB_LIGNES, vector<bool>(NB_COLL));
     
     initialisationGrilleJeu(grilleJeu);
     afficherGrilleJeu(grilleJeu);
 
-    
-    for(int i = 0; i <= 10; ++i){
-       
-    
+    for(int i = 0; i <= ITERATION; ++i){
+
       evolution(grilleJeu, anticipation);
       grilleJeu = anticipation;
       afficherGrilleJeu(grilleJeu);
     
-    
-
-    
-    
     }
-
-
 
     return EXIT_SUCCESS;
 }
 
 void initialisationGrilleJeu(vector<vector<bool>>& grilleJeu){
-
     grilleJeu[4][4] = true;
     grilleJeu[4][5] = true;
     grilleJeu[4][6] = true;
@@ -72,14 +76,12 @@ void initialisationGrilleJeu(vector<vector<bool>>& grilleJeu){
 void evolution(const vector<vector<bool>> &grilleJeu, vector<vector<bool>> &anticipation) {
    
    size_t col, ligne;
-   int iCompteur = 0;
-   
+
    for(size_t i = 0; i < ligne; i++){
-            for(size_t j = 0; j < col; j++){
-               
-               anticipation[i][j] = estVivant(grilleJeu, j, i);
-            }
-        }
+       for(size_t j = 0; j < col; j++){
+           anticipation[i][j] = estVivant(grilleJeu, j, i);
+       }
+   }
 }
 
 void afficherGrilleJeu(const vector<vector<bool>>& grilleJeu){
@@ -92,7 +94,7 @@ void afficherGrilleJeu(const vector<vector<bool>>& grilleJeu){
 
         for(size_t i = 0; i < ligne; i++){
             for(size_t j = 0; j < col; j++){
-                ecran += (grilleJeu[i][j] ? "x " : ". ");
+                ecran += string(1, grilleJeu[i][j] ? CAR_EN_VIE : CAR_MORT) + " ";
             }
             ecran += "\n\r";
         }
@@ -106,9 +108,11 @@ bool estVivant(const vector<vector<bool>> &grilleJeu, size_t posx, size_t posy )
    unsigned cpt = 0;
    
    bool estVivant = grilleJeu[posy][posx];
-   
-   for(int i = -1; i < 2; i++){
-      for(int j = -1; j < 2; j++){
+
+    int domaineDef = ZONE_INFLUANCE / 2;
+
+   for(int i = -domaineDef; i <= domaineDef; i++){
+      for(int j = -domaineDef; j <= domaineDef; j++){
          
          if((i != 0 || j != 0) 
                  && posy + i >=0 && posx + j >= 0
